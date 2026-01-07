8b625242b4b20a2d7f78504c8b1960c92128f301
Victor Requena <victor@vars.soy>
2026-01-07 02:35:23 -0400

    feat(ui): add theme implementation
    Implemented theming capabilities for light, dark and system modes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +24,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} flex min-h-dvh flex-col antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          disableTransitionOnChange
+          enableSystem
+          storageKey="DankMemerJournal_appTheme"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -1,30 +1,37 @@
 "use client";
 
-import { LogOutIcon, UserIcon } from "lucide-react";
+import {
+  CheckIcon,
+  LogOutIcon,
+  MoonIcon,
+  SunIcon,
+  SunMoonIcon,
+  UserIcon,
+} from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useTheme } from "next-themes";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Spinner } from "@/components/ui/spinner";
 import { authClient } from "@/lib/auth-client";
+import { Skeleton } from "./ui/skeleton";
 
 export function UserMenu() {
   const router = useRouter();
+  const { theme, setTheme } = useTheme();
   const { data: session, isPending } = authClient.useSession();
 
   if (isPending) {
-    return (
-      <Avatar>
-        <AvatarFallback>
-          <Spinner />
-        </AvatarFallback>
-      </Avatar>
-    );
+    return <Skeleton className="size-8 rounded-full" />;
   }
 
   if (!session) {
@@ -39,6 +46,12 @@ export function UserMenu() {
         },
       },
     });
+  }
+
+  function handleThemeSelection(mode: "light" | "dark" | "system") {
+    return () => {
+      setTheme(mode);
+    };
   }
 
   return (
@@ -60,6 +73,41 @@ export function UserMenu() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuLabel>{session.user.name}</DropdownMenuLabel>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            {theme === "dark" && <MoonIcon />}
+            {theme === "light" && <SunIcon />}
+            {theme === "system" && <SunMoonIcon />}
+            <span>Select Theme</span>
+          </DropdownMenuSubTrigger>
+          <DropdownMenuSubContent>
+            <DropdownMenuItem
+              onSelect={handleThemeSelection("light")}
+              className="cursor-pointer"
+            >
+              <SunIcon />
+              <span>Light</span>
+              {theme === "light" && <CheckIcon className="ml-auto" />}
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onSelect={handleThemeSelection("dark")}
+              className="cursor-pointer"
+            >
+              <MoonIcon />
+              <span>Dark</span>
+              {theme === "dark" && <CheckIcon className="ml-auto" />}
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onSelect={handleThemeSelection("system")}
+              className="cursor-pointer"
+            >
+              <SunMoonIcon />
+              <span>System</span>
+              {theme === "system" && <CheckIcon className="ml-auto" />}
+            </DropdownMenuItem>
+          </DropdownMenuSubContent>
+        </DropdownMenuSub>
+        <DropdownMenuSeparator />
         <DropdownMenuItem onSelect={handleSignOut} className="cursor-pointer">
           <LogOutIcon />
           <span>Sign Out</span>
