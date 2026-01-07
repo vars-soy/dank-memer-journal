e97e4b2952b40939e03045e58baeb9da9b4e6b53
Victor Requena <victor@vars.soy>
2026-01-07 10:50:15 -0400

    feat(db): add local database development capabilities
    Added a local Postgres DB that uses Neon's local HTTP proxy using Docker
    
    See: https://neon.com/guides/local-development-with-neon#local-postgresql

--- lib/db/client.ts
+++ lib/db/client.ts
@@ -1,6 +1,22 @@
+import { neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@/generated/prisma/client";
 import { env } from "@/lib/env/server";
+import ws from "ws";
+
+if (process.env.NODE_ENV === "development") {
+  neonConfig.fetchEndpoint = (host) => {
+    const [protocol, port] =
+      host === "db.localtest.me" ? ["http", 4444] : ["https", 443];
+    return `${protocol}://${host}:${port}/sql`;
+  };
+  const connectionStringUrl = new URL(env.DATABASE_URL);
+  neonConfig.useSecureWebSocket =
+    connectionStringUrl.hostname !== "db.localtest.me";
+  neonConfig.wsProxy = (host) =>
+    host === "db.localtest.me" ? `${host}:4444/v2` : `${host}/v2`;
+}
+neonConfig.webSocketConstructor = ws;
 
 const adapter = new PrismaNeon({ connectionString: env.DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
